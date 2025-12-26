c7fef4581541580895868831790eb2eb8e45aeff
Nashwa-art <shekoobts.com@gmail.com>
2025-12-26 02:49:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 function getResult() {
     let seat = document.getElementById("seat").value;
-
+    document.getElementById("loading").innerText = "Loading...";
+    
     fetch("https://script.google.com/macros/s/AKfycbyuHqno-9AdTVy8GmOUXQD3M6qxFOTizTT7rv1tzR4MGSr2gypCoNK3UQ-jeFh0Buxq3w/exec?seat=" + seat)
     .then(res => res.json())
     .then(data => {
+        document.getElementById("loading").innerText = "";
+
         if (data.error) {
             document.getElementById("name").innerText = "لا يوجد طالب بهذا الرقم";
-            document.getElementById("math").innerText = "";
-            document.getElementById("science").innerText = "";
-            document.getElementById("arabic").innerText = "";
-            document.getElementById("english").innerText = "";
-            document.getElementById("social").innerText = "";
-            document.getElementById("it").innerText = "";
-            document.getElementById("religion").innerText = "";
-            document.getElementById("beehive").innerText = "";
-            document.getElementById("quran").innerText = "";
-            document.getElementById("total").innerText = "";
-            document.getElementById("G/F").innerText = "";
+            clearFields();
             return;
         }
 
-        document.getElementById("name").innerText = "Name : " + data.name;
-        document.getElementById("math").innerText = "Maths : " + data.math;
-        document.getElementById("science").innerText = "Science : " + data.science;
-        document.getElementById("arabic").innerText = "Arabic : " + data.arabic;
-        document.getElementById("english").innerText = "English : " + data.english;
-        document.getElementById("social").innerText = "Social Studies : " + data.social;
-        document.getElementById("total").innerText = "Total: " + data.total;
-        document.getElementById("it").innerText = "IT : " + data.it;
-        document.getElementById("religion").innerText = "Religion : " + data.religion;
-        document.getElementById("beehive").innerText = "Beehive : " + data.beehive;
-        document.getElementById("quran").innerText = "Quran : " + data.quran;
-        document.getElementById("G/F").innerText = "G/F : " + data.language2;
-        
+        document.getElementById("name").innerText = "Name: " + data.name;
+        document.getElementById("math").innerText = data.math;
+        document.getElementById("science").innerText = data.science;
+        document.getElementById("arabic").innerText = data.arabic;
+        document.getElementById("english").innerText = data.english;
+        document.getElementById("social").innerText = data.social;
+        document.getElementById("it").innerText = data.it;
+        document.getElementById("religion").innerText = data.religion;
+        document.getElementById("beehive").innerText = data.beehive;
+        document.getElementById("quran").innerText = data.quran;
+        document.getElementById("total").innerText = data.total;
     });
 }
+
+function clearFields() {
+    const fields = ["math", "science", "arabic", "english", "social", "it", "religion", "beehive", "quran", "total"];
+    fields.forEach(id => document.getElementById(id).innerText = "");
+}
+
